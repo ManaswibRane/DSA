4bee65601a0c10babe3e261cb2dd2a31aa83611f
ManaswibRane <manaswirane1947@gmail.com>
2025-03-02 03:53:07 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (7.69%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.cpp
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -5,10 +5,8 @@ public:
         int ct=n-1;
         vector<int> ans(n,0);
         for(int i=0;i<n;i++){
-            if(nums[i]%2!=0){
-                ans[ct--]=1;
-            }
-        }
-        return ans;
+        if(nums[i]%2!=0) ans[ct--]=1;
+ 
+}    return ans;
     }
 };
