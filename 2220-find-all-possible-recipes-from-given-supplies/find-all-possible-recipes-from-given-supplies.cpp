f295469b14cd982029a2261f9609a79fc5ea026c
ManaswibRane <manaswirane1947@gmail.com>
2025-03-22 00:04:48 +0530

    Time: 1128 ms (5.05%) | Memory: 311.6 MB (5.25%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -2,52 +2,37 @@ class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& rec, vector<vector<string>>& ingre, vector<string>& supplies) {
         vector<string> ans;
-        unordered_set<string> supp(supplies.begin(), supplies.end());
-        unordered_map<string, vector<string>> unprep;
-        unordered_set<string> pending; // Keeps track of recipes that need to be rechecked
-
-        // First pass: Identify directly possible recipes & store unprepared ones
-        for (int i = 0; i < rec.size(); i++) {
-            bool all_ing = true;
-            for (string& ing : ingre[i]) {
-                if (supp.find(ing) == supp.end()) {
-                    all_ing = false;
-                    unprep[rec[i]].push_back(ing);
-                }
-            }
-            if (all_ing) {
-                ans.push_back(rec[i]);
-                supp.insert(rec[i]);
-            } else {
-                pending.insert(rec[i]);
-            }
+        int rs=rec.size();
+        int is=ingre.size();
+        int s=supplies.size();
+        int count=rs;
+        unordered_set<string> supp;
+        vector<bool> done(count,false);
+       
+        for(int i=0;i<s;i++){
+           
+             supp.insert(supplies[i]);
         }
+        unordered_map<string,vector<string>> unprep;
+         while(count--){
+        for(int i=0;i<rs;i++){
+             if(done[i]) continue;
+            bool alling=true;
+            for(int j=0;j<ingre[i].size();j++){
+                if (supp.find(ingre[i][j])==supp.end()){
+                    alling=false;
+                   unprep[rec[i]].push_back(ingre[i][j]);
 
-        // Second pass: Iteratively check unprepared recipes
-        bool progress = true;
-        while (progress) {
-            progress = false;
-            for (auto it = pending.begin(); it != pending.end();) {
-                string recipe = *it;
-                vector<string>& missing = unprep[recipe];
-
-                // Remove available ingredients
-                missing.erase(remove_if(missing.begin(), missing.end(), [&](const string& ing) {
-                    return supp.find(ing) != supp.end();
-                }), missing.end());
-
-                // If all ingredients are now available, add recipe to answer
-                if (missing.empty()) {
-                    ans.push_back(recipe);
-                    supp.insert(recipe);
-                    it = pending.erase(it); // Erase from pending
-                    progress = true;  // Keep iterating
-                } else {
-                    ++it;
-                }
+                } 
+               
             }
+            if(alling){
+                    ans.push_back(rec[i]);
+                    supp.insert(rec[i]);
+                    done[i]=true;
+                }
         }
-
-        return ans;
+    }
+     return ans;
     }
 };
