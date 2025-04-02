0555c151e2999514a71b7045ba8445302b37a737
ManaswibRane <manaswirane1947@gmail.com>
2025-04-03 02:14:04 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (35.19%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> bigfront(n,0),bigrear(n,0);
-          bigfront[0]=nums[0];
-        for(int i=1;i<n;i++){
-             bigfront[i]=max(nums[i-1],bigfront[i-1]);
-           
-        }
-        bigrear[n-1]=nums[n-1];
-         for(int i=n-2;i>=0;i--){
-             bigrear[i]=max(nums[i],bigrear[i+1]);
-            //  bigrear[n-i-1]=max(nums[n-i],bigfront[n-i]);
-        }
+        int n=nums.size(),idxm=1,idx=0;
+        if(n<3) return 0;
+        long long maxdiff=0,maxi=0;
         long long res=0;
-        for(int j=1;j<n-1;j++){
-               res=max(res,(long long)(bigfront[j]-nums[j])*bigrear[j+1]);
-               cout<<res<<"\t"<<bigfront[j]<<"\t"<<nums[j]<<"\t"<<bigrear[j+1]<<endl;
+        for(int k=0;k<n;k++){
+           res=max(res,(long long)(maxdiff)*nums[k]);
+           maxdiff=max(maxi-nums[k],maxdiff);
+           maxi=max(maxi,(long long)nums[k]);
         }
         
            return res;
