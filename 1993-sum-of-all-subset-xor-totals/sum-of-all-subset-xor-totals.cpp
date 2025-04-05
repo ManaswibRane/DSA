325b649c8f553a1e2e8f4d3d3e9d12cd637245fa
ManaswibRane <manaswirane1947@gmail.com>
2025-04-05 19:32:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (71.99%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-   void solve(vector<int>& nums,vector<vector<int>> &res,int i,int n,vector<int> &tar){
+   int solve(vector<int>& nums,int i,int n,int  X){
         if(i>=n){
-            res.push_back(tar);
-            return;
+            return X;
         }
-        tar.push_back(nums[i]);
-         solve(nums,res,i+1,n,tar);
-         tar.pop_back();
-         solve(nums,res,i+1,n,tar);
+        
+       int include=solve(nums,i+1,n,X^nums[i]);
+    
+         int excludes=solve(nums,i+1,n,X);
+         return include+excludes;
    }
-    vector<vector<int>> subsets(vector<int>& nums) {
+    int subsets(vector<int> &nums) {
         int n=nums.size();
-        vector<vector<int>> res;
-        vector<int> tar;
-        solve(nums,res,0,n,tar);
-        return res;
+       return solve(nums,0,n,0);
+       
     }
     int subsetXORSum(vector<int>& nums) {
-        int ans=0;
-        vector<vector<int>> subset=subsets(nums);
-        for(int i=0;i<subset.size();i++){
-            int x=0;
-            for(int j=0;j< subset[i].size();j++){
-              x= x ^ subset[i][j];
-            }
-            ans+=x;
-        }
-        return ans;
+      return subsets(nums) ;
        
     }
 };
