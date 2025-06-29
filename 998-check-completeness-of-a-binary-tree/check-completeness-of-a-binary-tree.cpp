fbfdc2f931afd69af7f95028c81e8d08779de6cf
ManaswibRane <manaswirane1947@gmail.com>
2025-06-30 03:45:07 +0530

    Time: 11 ms (1.57%) | Memory: 13.8 MB (25.90%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -9,43 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- 
-
 class Solution {
 public:
-    bool bfs(TreeNode* root) {
-        if (!root) return 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        int deepest = 0;
-        bool noChild=false;
-        while (!q.empty()) {
-            int n = q.size();
-            while (n--) {
-                TreeNode* node = q.front();
-                q.pop();
-
-                    if (node->left) {
-                if (noChild) return false;
-                q.push(node->left);
-            } else {
-                noChild = true; 
-            }
-
-            if (node->right) {
-                if (noChild) return false; 
-                q.push(node->right);
-            } else {
-                noChild = true; 
-            }
-            }  
-        }
-
-        return true;
-    }
-
     bool isCompleteTree(TreeNode* root) {
-    
-        return  bfs(root);
+        queue<TreeNode*> q;
+        bool pehleKa=false;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* node=q.front();
+            q.pop();
+            if(node==NULL){
+                pehleKa=true;
+            }else{ if(pehleKa){
+                return false;
+              }
+              
+            }
+             if(node!=NULL){
+                   q.push(node->left);
+                  q.push(node->right);
+              }
+             
+             
+              cout<<pehleKa<<endl;
+        }
+        return true;
     }
 };
