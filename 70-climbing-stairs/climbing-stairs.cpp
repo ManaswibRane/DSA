7f3ada39759448c890a387f77f8a83b078207a00
ManaswibRane <manaswirane1947@gmail.com>
2025-07-04 23:42:28 +0530

    Time: 2 ms (5.21%) | Memory: 8.5 MB (51.63%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-     int t[46];
-     int solve(int n){
-       
-        if(n<0){
-            return 0;
-        }
-        if(n==0){
-            return 1;
-        }
-         if(t[n]!=-1){
-            return t[n];
-        }
-       int ones=solve(n-1);
-       int twos=solve(n-2);
-        
-       return t[n]= ones+twos;
-
-     }
-
+vector<int> dp;
+int solve(int n){
+     if(n<0) return 0;
+        if(n==0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int ans1=solve(n-1);
+        int ans2=solve(n-2);
+        return dp[n]=ans1+ans2;
+}
     int climbStairs(int n) {
-       memset(t,-1,sizeof(t));
-        return solve(n);  
+        dp.resize(n+1,-1);
+        return solve(n);
     }
 };
