ed7a806af6891e4ce3e5209b3b9c026affed3fe2
ManaswibRane <manaswirane1947@gmail.com>
2025-07-16 06:06:21 +0530

    Time: 51 ms (56.41%) | Memory: 62.3 MB (38.89%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool unique(string s1,string s2){
+        vector<int> val(26,0);
+        for(char ch: s2){
+            if(val[ch-'a']>0) return true;
+            val[ch-'a']++;
+        }
+       for(char ch: s1){
+            if(val[ch-'a']>0) return true;
+            val[ch-'a']++;
+        }
+        return false;
+
+    }
+    int solve(int idx,int n,vector<string>& arr,string temp){
+          if(idx>=n) return temp.length();
+           int take=0,notTake=0;
+          if(unique(temp,arr[idx])){
+                notTake=solve(idx+1,n,arr,temp);
+          }
+          else{
+            take=solve(idx+1,n,arr,temp+arr[idx]);
+            notTake=solve(idx+1,n,arr,temp);
+          }
+          return max(take,notTake);
+    }
+    int maxLength(vector<string>& arr) {
+          int n=arr.size();
+          return solve(0,n,arr,"");
+    }
+};
