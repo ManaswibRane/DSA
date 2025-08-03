a8c4967dc0b8754bbb184afc36c1f3ff1c783a45
ManaswibRane <manaswirane1947@gmail.com>
2025-08-03 21:59:01 +0530

    Time: 543 ms (54.93%) | Memory: 158.7 MB (65.75%) - LeetSync

--- 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.cpp
+++ 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
 typedef long long ll;
     int numberOfArithmeticSlices(vector<int>& nums) {
-         int n=nums.size();
-         unordered_map<ll,ll> mp[n];
-         ll res=0;
-         for(int i=0;i<n;i++){
-              for(int j=0;j<i;j++){
-                ll diff=(ll)nums[i]-(ll)nums[j];
-                   ll count=0;
-
-               auto it=mp[j].find(diff);
-                 if(it!=mp[j].end()){
-                       count=mp[j][diff];
-                        res+=count;
-                      
-             }
-                  mp[i][diff]+=count+1;
-                
-              }
-         }
-         return res;
+       int n=nums.size();
+        int ans=0;
+       vector<unordered_map<ll, ll>> mp(n);
+       for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                ll diff = (ll)nums[i]-(ll)nums[j];
+                auto it=mp[j].find(diff);
+                ll ctatj;
+                if(it!=mp[j].end()){
+                     ctatj=it->second;
+                }
+                else{
+                    ctatj=0;
+                }
+                mp[i][diff]+=ctatj+1;
+                ans+=ctatj;
+            }
+       }
+       return ans;
     }
 };
