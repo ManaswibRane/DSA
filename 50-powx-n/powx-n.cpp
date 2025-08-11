5ddc635f56502c815165efff592e0b6de16cf25f
ManaswibRane <manaswirane1947@gmail.com>
2025-08-11 18:36:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.47%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double a, int n) {
+        double ans = 1;
+        long long pow=n;
+        if(pow<0) {
+            a=1/a;
+             pow=-pow;
+        }
+       
+        while(pow>0){
+            if((pow&1)!=0){
+                ans=ans*a;
+
+            }
+            a=a*a;
+            pow=pow>>1;
+        }
+        return ans;
+    }
+};
