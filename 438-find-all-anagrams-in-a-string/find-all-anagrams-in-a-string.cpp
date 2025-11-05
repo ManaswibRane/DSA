12197f32d431212bd8a8eb10ee775fca0243587c
ManaswibRane <manaswirane1947@gmail.com>
2025-11-05 12:49:07 +0530

    Time: 111 ms (10.57%) | Memory: 17.4 MB (12.34%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string txt, string pat) {
-                 unordered_map<char,int> count;
-         vector<int> ans;
-         int t=txt.length(),p=pat.length();
-        for(int i=0;i<p;i++){
-            count[pat[i]]++;
+    vector<int> findAnagrams(string s, string p) {
+        map<char, int> m1;
+        map<char, int> m2;
+        vector<int> a;
+        for(int i=0;i<p.size();i++){
+            m1[p[i]]++;
         }
-         int i=0,j=0;
-         while(j<t){
-             count[txt[j]]--;
-             if( count[txt[j]]==0){
-                 count.erase(txt[j]);
-             }
-             
-             if(j-i+1==p){
-                if(count.empty()) ans.push_back(i);
-               
-                 count[txt[i]]++;
-                if (count[txt[i]] == 0) count.erase(txt[i]);
-                 i++;
-             }
-             j++;
-         }
-         return ans;
+        for(int i=0;i<p.size();i++){
+            m2[s[i]]++;
+        }
+        if(m1==m2){
+            a.push_back(0);
+        }
+        for(int i=p.size();i<s.size();i++){
+            m2[s[i]]++;
+            m2[s[i-p.size()]]--;
+            if(m2[s[i - p.size()]] == 0)
+                m2.erase(s[i - p.size()]);
+            if(m1==m2){
+                a.push_back(i-p.size()+1);
+                cout<<"i "<<i<<endl;
+            }
+
+        }
+        return a;
+
+
     }
 };
